e6dd42485426eb5421ec3df40b3aa867dea02514
Jerika Kanz <jerikacapila@gmail.com>
2021-11-23 13:53:49 +0100

    02 Diff Two Arrays

---
+++ 02-diff-2-arrays.js
@@ -0,0 +1,15 @@
+/*
+Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
+In other words, return the symmetric difference of the two arrays.
+
+Note: You can return the array with its elements in any order.
+*/
+
+function diffArray(arr1, arr2) {
+  return [
+    ...arr1.filter((x) => !arr2.includes(x)),
+    ...arr2.filter((x) => !arr1.includes(x)),
+  ];
+}
+
+console.log(diffArray([1, "calf", 3, "piglet"], [7, "filly"]));
