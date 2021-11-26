2daeb014780076b8a80ea66c776d08e6f54797c3
Jerika Kanz <jerikacapila@gmail.com>
2021-11-26 15:21:56 +0100

    11 Convert HTML Entities

--- 11-html-entities.js
+++ 11-html-entities.js
@@ -1,3 +1,20 @@
 /*
 Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
 */
+
+function convertHTML(str) {
+  return String(str)
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
+console.log(convertHTML("Dolce & Gabbana"));
+console.log(convertHTML("Hamburgers < Pizza < Tacos"));
+console.log(convertHTML("Sixty > twelve"));
+console.log(convertHTML('Stuff in "quotation marks"'));
+console.log(convertHTML("Schindler's List"));
+console.log(convertHTML("<>"));
+console.log(convertHTML("abc"));
